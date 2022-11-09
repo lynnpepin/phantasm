585ce78634d397f5f1a1546f2ede0872743e9ef9
lynn pepin <lynn@lynndotpy.xyz>
2022-11-09 18:01:37 -0500

    Basic loop with string working

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
+use std::io::{stdin,stdout,Write};
+
 fn main() {
+    loop {
+        print!(">>> ");
+        let ss:String = String::from("hello"); //input();
+        print!("\n... {}\n", ss);
+    }
 }
+
+/*
+todos
+ - loop over input, printing response
+ - tokenize each line
+ - implement 'set x 0'
+ - use arbitrary precision for each value
+*/
