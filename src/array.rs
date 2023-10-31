b375ca629741aa9945fb615b03ebc42d7ee71bee
lynn pepin <lynnpepin@tutanota.com>
2023-10-31 17:31:50 -0400

    wip: Cleanup array stuff and debug like a flawful cowboy

---
+++ src/array.rs
@@ -0,0 +1,226 @@
+use std::collections::HashMap;
+use crate::number::Number;
+use std::num::ParseIntError;
+
+// Parse comma-separated string to numbers
+pub fn parse_numbers(ss: &String) -> Result<Vec<Number>, ()> {
+  //Helper function to parse a comma-separated list of numbers
+  // If any of the numbers fail to parse, return Err ()
+  // Todo: Clean this up, better E
+  let mut vec: Vec<Number> = Vec::new();
+  for token in ss.split(",") {
+    if let Ok(num) = token.parse::<i64>() {
+      vec.push(Number::I64(num));
+    } else if let Ok(num) = token.parse::<f64>() {
+      vec.push(Number::F64(num));
+    } else {
+      //println!("parse_numbers({}): Could not parse {} as number", ss, token);
+      return Err(())
+    }
+  }
+  Ok(vec)
+}
+
+// Helper func for state[key][idx]
+pub fn get_value_from_state(
+  state: &HashMap<String, Vec<Number>>,
+  key: String,
+  idx: Option<usize>, 
+) -> Result<Number, ()> {
+  // Option instead of default args: stackoverflow.com/questions/24047686/
+  state.get(&key)
+  .map(|vec| vec[idx.unwrap_or(0)])
+  .ok_or(())
+}
+
+// Interpret value as literal or variable embedded in state
+// e.g. get_value(kk, state) -> 3, or get_value(kk.0, state) -> 3, or get_value(ll.4, state) -> 1.414
+pub fn get_value(
+  token: String, state: &HashMap<String, Vec<Number>>
+) -> Result<Number, ()> {
+  /*
+  Helper function to read token from state.
+  `token` can be one of:
+  1. Literal, does not touch state:
+  1. Parse as i64
+  2. Parse as f64
+  
+  2. Variable, stored in state:
+  1. key.idx, indexing array
+  2. key (same as key.0), reading a variable
+  */
+  if let Ok(val) = token.parse::<i64>() {
+    Ok(Number::I64(val))
+  } else if let Ok(val) = token.parse::<f64>() {
+    Ok(Number::F64(val))
+  } else {
+    // Read "and then" as "and then, if that works,"
+    // Split token into key.index
+    
+    // token = key.index_str
+    if let Some((key, index_str)) = token.split_once('.') {
+      match get_value_from_state(state, key.to_string(), index_str.parse::<usize>().ok()) {
+        Ok(num) => Ok(num),
+        Err(_) => {
+          //println!("get_value({}, {:?}): Could not parse token", token, state);
+          Err(())
+        }
+      }
+    } else {
+      get_value_from_state(state, token, None)
+    }
+  }
+}
+
+// state[key][index] = value
+pub fn set_value_in_vec_in_state(
+  state: &mut HashMap<String, Vec<Number>>,
+  key: String,
+  idx: Option<usize>, 
+  value: Number,
+) -> Result<(), String> {
+  match state.get_mut(&key) {
+    // state[key] exists; update it
+    Some(vec) => {
+      vec[idx.unwrap_or(0)] = value;
+      Ok(())
+    },
+    // state[key] doesn't exist; create it
+    None => {
+      let mut vec: Vec<Number> = Vec::new();
+      vec.resize(idx.unwrap_or(0) + 1, Number::I64(0));
+      vec[idx.unwrap_or(0)] = value;
+      Ok(())
+    }
+  }
+}
+
+// set state[key][idx:] = values, overwriting
+pub fn set_value_in_state(
+  state: &mut HashMap<String, Vec<Number>>,
+  key: String,
+  idx: Option<usize>, 
+  values: Vec<Number>,
+) -> Result<(), String> {
+  /*
+  Set state[key][idx:] = value,
+  e.g. with state["foo"] = [0, 10, 20, 30],
+  set_value_in_state("foo", 3, [1, 2, 3, 4, 5])
+  yields state["foo"] = [0, 10, 20, 1, 2, 3, 4, 5]
+  */
+  let index = idx.unwrap_or(0);
+  //println!("set_value_in_state({}, {}, {:?})", key, index, values);
+  match state.get_mut(&key) {
+    // state[key] exists; update it
+    Some(vec) => {
+      // Resize if it's too small
+      if index + values.len() > vec.len() {
+        vec.resize(
+          index + values.len(),
+          Number::I64(0)
+        )
+      }
+      // overwrite vec[idx:idx+values.len()] with values
+      for (ii, vv) in values.into_iter().enumerate() {
+        vec[index + ii] = vv;
+      }
+      
+      Ok(())
+    },
+    // state[key] doesn't exist; create it
+    None => {
+      // todo: This duplicates above code, move to a separate function?
+      let mut vec: Vec<Number> = Vec::new();
+      if index + values.len() > vec.len() {
+        vec.resize(
+          index + values.len(),
+          Number::I64(0)
+        )
+      }
+      // overwrite vec[idx:idx+values.len()] with values
+      for (ii, vv) in values.into_iter().enumerate() {
+        vec[index + ii] = vv;
+      }
+      state.insert(
+        key.to_string(),
+        vec
+      );
+      Ok(())
+    }
+  }
+}
+
+// Set state[token] = [val1, val2, ...] or state[key][idx] = [val1, val2, ...],
+pub fn set_value(
+  state: &mut HashMap<String, Vec<Number>>,
+  token: String,
+  value: String,
+) -> Result<(), &str> {
+  /*
+  Helper function to set state[token] = value
+  Four cases for token, value:
+  1. `key.idx, number`, i.e. state[key][index] = [number,]
+  2. `key
+  
+  
+  Three cases for token, value:
+  1. "key.idx", "num":      state[token][idx] = [number]
+  2. "token", "num":          state[token] = [number]
+  3. "token", "num1,num2", ...: state[token] = [num1, num2, ...]
+  
+  Consider handling fourth  case:
+  4. "key.idx", "num1,num2"
+  
+  Should probably be `Option<>` or something
+  */
+  match parse_numbers(&value) {
+    Ok(values) => {
+      //println!(".. values: {:?}", values);
+      match token.split_once('.') {
+        Some((key, index_str)) => {
+          //println!(".. .. key: {}, index_str: {}", key, index_str);
+          match index_str.parse::<usize>() {
+            Ok(idx) => {
+              set_value_in_state(
+                state,
+                key.to_string(),
+                Some(idx),
+                values
+              );
+              Ok(())
+            },
+            Err(ParseIntError) => {
+              //println!(
+              //  "set_value({:?}, {}, {}): Could not parse {} in set_value call",
+              //  state, token, value, index_str
+              //);
+              Err("Could not parse index in set_value call")
+            }
+            
+          }
+        },
+        // Handle the cases 2, 3
+        None => {
+          match values.len() {
+            0 => {
+              //println!("Could not parse empty values");
+              Err("Could not parse empty values")
+            },
+            _ => {
+              state.insert(
+                token.to_string(),
+                values
+              );
+              Ok(())
+            }
+          }
+        }
+      }
+    },
+    Err(_) => {
+      // Can't parse `token` as a number
+      //println!("set_value({}, {}): Could not parse values", token, value);
+      Err("Could not parse values in set_value call")
+    },
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::io::{stdin, stdout, Write};
 mod number;
 use number::Number;
+mod array;
+use array::{parse_numbers, get_value_from_state, get_value, set_value};
 
 // Get input string from stdin
 fn input() -> String {
@@ -19,215 +21,6 @@ fn input() -> String {
   }
   
   ss
-}
-
-// Parse comma-separated string to numbers
-fn parse_numbers(ss: &String) -> Result<Vec<Number>, ()> {
-  //Helper function to parse a comma-separated list of numbers
-  // If any of the numbers fail to parse, return Err ()
-  // Todo: Clean this up, better E
-  let mut vec: Vec<Number> = Vec::new();
-  for token in ss.split(",") {
-    if let Ok(num) = token.parse::<i64>() {
-      vec.push(Number::I64(num));
-    } else if let Ok(num) = token.parse::<f64>() {
-      vec.push(Number::F64(num));
-    } else {
-      println!("Could not parse {} as number", token);
-      return Err(())
-    }
-  }
-  Ok(vec)
-}
-
-// Helper func for state[key][idx]
-fn get_value_from_state(
-  state: &HashMap<String, Vec<Number>>,
-  key: String,
-  idx: Option<usize>, 
-) -> Result<Number, ()> {
-  // Option instead of default args: stackoverflow.com/questions/24047686/
-  state.get(&key)
-  .map(|vec| vec[idx.unwrap_or(0)])
-  .ok_or(())
-}
-
-// Interpret value as literal or variable embedded in state
-// e.g. get_value(kk, state) -> 3, or get_value(kk.0, state) -> 3, or get_value(ll.4, state) -> 1.414
-fn get_value(
-  token: String, state: &HashMap<String, Vec<Number>>
-) -> Result<Number, ()> {
-  /*
-  Helper function to read token from state.
-  `token` can be one of:
-  1. Literal, does not touch state:
-  1. Parse as i64
-  2. Parse as f64
-  
-  2. Variable, stored in state:
-  1. key.idx, indexing array
-  2. key (same as key.0), reading a variable
-  */
-  if let Ok(val) = token.parse::<i64>() {
-    Ok(Number::I64(val))
-  } else if let Ok(val) = token.parse::<f64>() {
-    Ok(Number::F64(val))
-  } else {
-    // Read "and then" as "and then, if that works,"
-    // Split token into key.index
-    
-    // token = key.index_str
-    if let Some((key, index_str)) = token.split_once('.') {
-      match get_value_from_state(state, key.to_string(), index_str.parse::<usize>().ok()) {
-        Ok(num) => Ok(num),
-        Err(_) => {
-          println!("Could not parse token {} in get_value call", token);
-          Err(())
-        }
-      }
-    } else {
-      get_value_from_state(state, token, None)
-    }
-  }
-}
-
-// state[key][index] = value
-fn set_value_in_vec_in_state(
-  state: &mut HashMap<String, Vec<Number>>,
-  key: String,
-  idx: Option<usize>, 
-  value: Number,
-) -> Result<(), String> {
-  match state.get_mut(&key) {
-    // state[key] exists; update it
-    Some(vec) => {
-      vec[idx.unwrap_or(0)] = value;
-      Ok(())
-    },
-    // state[key] doesn't exist; create it
-    None => {
-      let mut vec: Vec<Number> = Vec::new();
-      vec.resize(idx.unwrap_or(0) + 1, Number::I64(0));
-      vec[idx.unwrap_or(0)] = value;
-      Ok(())
-    }
-  }
-}
-
-// set state[key][idx:] = values, overwriting
-fn set_value_in_state(
-  state: &mut HashMap<String, Vec<Number>>,
-  key: String,
-  idx: Option<usize>, 
-  values: Vec<Number>,
-) -> Result<(), String> {
-  /*
-  Set state[key][idx:] = value,
-  e.g. with state["foo"] = [0, 10, 20, 30],
-  set_value_in_state("foo", 3, [1, 2, 3, 4, 5])
-  yields state["foo"] = [0, 10, 20, 1, 2, 3, 4, 5]
-  */
-  let index = idx.unwrap_or(0);
-  println!("set_value_in_state({}, {}, {:?})", key, index, values);
-  match state.get_mut(&key) {
-    // state[key] exists; update it
-    Some(vec) => {
-      // Resize if it's too small
-      if index <= (vec.len() + values.len()) {
-        vec.resize(
-          index + values.len() + 1,
-          Number::I64(0)
-        )
-      }
-      // overwrite vec[idx:idx+values.len()] with values
-      vec.splice(
-        index..index + values.len(),
-        values.into_iter()
-      );
-      
-      Ok(())
-    },
-    // state[key] doesn't exist; create it
-    None => {
-      state.insert(
-        key.to_string(),
-        values
-      );
-      Ok(())
-    }
-  }
-}
-
-// Set state[token] = [val1, val2, ...] or state[key][idx] = [val1, val2, ...],
-fn set_value(
-  state: &mut HashMap<String, Vec<Number>>,
-  token: String,
-  value: String,
-) -> Result<(), &str> {
-  /*
-  Helper function to set state[token] = value
-  Four cases for token, value:
-  1. `key.idx, number`, i.e. state[key][index] = [number,]
-  2. `key
-  
-  
-  Three cases for token, value:
-  1. "key.idx", "num":      state[token][idx] = [number]
-  2. "token", "num":          state[token] = [number]
-  3. "token", "num1,num2", ...: state[token] = [num1, num2, ...]
-  
-  Consider handling fourth  case:
-  4. "key.idx", "num1,num2"
-  
-  Should probably be `Option<>` or something
-  */
-  println!("set_value({}, {})", token, value);
-  match parse_numbers(&token) {
-    Ok(values) => {
-      println!(".. values: {:?}", values);
-      match token.split_once('.') {
-        Some((key, index_str)) => {
-          println!(".. .. key: {}, index_str: {}", key, index_str);
-          match index_str.parse::<usize>() {
-            Ok(idx) => {
-              set_value_in_state(
-                state,
-                key.to_string(),
-                Some(idx),
-                values
-              );
-              Ok(())
-            },
-            Err(ParseIntError) => {
-              println!("Could not parse {} in set_value call", index_str);
-              Err("Could not parse index in set_value call")
-            }
-            
-          }
-        },
-        // Handle the cases 2, 3
-        None => {
-          match values.len() {
-            0 => {
-              println!("Could not parse empty values");
-              Err("Could not parse empty values")
-            },
-            _ => {
-              state.insert(
-                token.to_string(),
-                values
-              );
-              Ok(())
-            }
-          }
-        }
-      }
-    },
-    Err(_) => {
-      println!("Could not parse values in set_value call");
-      Err("Could not parse values in set_value call")
-    },
-  }
 }
 
 
@@ -252,10 +45,10 @@ fn main() {
       ["set", kk, vv] => {
         set_value(&mut state, kk.to_string(), vv.to_string());
       },
-      /*
       ["del", kk] => {
         state.remove(&kk.to_string());
       },
+      /*
       ["print"] => { println!("{:?}", state) },
       ["print", kk] => { println!("{:?}", get_value(kk, &state)); },
       // todo: need to figure out how to just derive all of these from primitive
@@ -359,6 +152,7 @@ Longterm things:
 - Stricter grammar 
 - Proper compiler
 - Test driven dev
+- Clean up the errors, printouts
 - Docstrings
 - Review all the functions, redundancy, etc.
 
